d2bc2eede569031f3cfce475ed7aae14ae5f2400
chedwards492 <chedwards492@gmail.com>
2023-09-18 16:32:52 -0700

    about done with front page
    responsive, can copy link, no delete button, has summary, but not functioning summary

--- cart/cart.js
+++ cart/cart.js
@@ -5,3 +5,15 @@ function onButtonClick() {
 }
 
 btn.addEventListener('click', onButtonClick);
+
+
+
+/* this alerts properly but the link does not acc get copied to my clipboard, might just be b/c it's a live server tho so idk */
+function copyLink(val) {
+    let copiedLink = val.parentElement.parentElement.firstElementChild.getAttribute("href");
+    //alert("HELLOOOO" + copiedLink);
+
+    navigator.clipboard.writeText(copiedLink);
+
+    alert("copied the link: " + copiedLink);
+}
